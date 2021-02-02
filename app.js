034437a8c4dbe7a306b891bc50f50f11c08c9f1a
Ricky Yu <rickyyu2294@gmail.com>
2021-02-01 17:51:19 -0800

    Connect Database

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-const app = express();
 const path = require('path');
+const ChickenCoop = require("./models/chickenCoop")
 
+mongoose.connect('mongodb://localhost:27017/chicken-coop', {
+  useNewUrlParse: true,
+  useCreateIndex: true,
+  useUnifiedParser: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
+const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function (req, res) {
   res.render('home');
 })
+
+app.get('/chickencoop', (req, res) => {
+  const coop = new ChickenCoop{
+    name = 'MyFirstCoop',
+    description = 'where my chickens live'
+  }
+
+  await coop.save();
+  res.send(coop);
+});
  
 app.listen(3000, () => {
     console.log("Listening on port 3000");
--- models/chickenCoop.js
+++ models/chickenCoop.js
@@ -3,5 +3,7 @@ const Schema = mongoose.Schema;
 
 const CoopSchema = new Schema({
     name: String,
-    
+    description: String,
 });
+
+module.exports = mongoose.Model('Coop', CoopSchema);
