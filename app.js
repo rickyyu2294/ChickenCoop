11eca640754facc54e5b77ef10915cf7237b37ee
Ricky Yu <rickyyu2294@gmail.com>
2021-02-22 00:56:36 -0800

    Configure sessions

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const ejsMate = require('ejs-mate');
 const CoopRoutes = require('./routes/coopsRoutes.js');
 const ChickenRoutes = require('./routes/chickenRoutes.js')
 const ExpressError = require('./utils/ExpressError');
-const Session = require('express-session');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/chicken-coop', {
   useNewUrlParser: true,
@@ -29,9 +29,17 @@ const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: true,
+}
+app.use(session(sessionConfig));
 
 // Routes 
 
