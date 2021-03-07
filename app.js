4e115fbfd8af1971e6ffd3a7b61dbcd61b9e9828
Ricky Yu <rickyyu2294@gmail.com>
2021-03-07 14:20:28 -0800

    Server static files from public folder

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true}));
 
