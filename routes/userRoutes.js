b61081a80a498cf76cf248225686276a705ee96b
Ricky Yu <rickyyu2294@gmail.com>
2021-03-10 20:30:36 -0800

    Create login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,18 @@ const Joi = require("joi");
 const ExpressError = require('../utils/ExpressError');
 const router = express.Router({mergeParams: true});
 const catchAsync = require("../utils/CatchAsync");
+const passport = require('passport');
 
 const User = require('../models/userModel');
+
+// User registration
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
 router.post('/register', catchAsync(async(req, res) => {
-    const {email, username, password} = req.body.user;
+    const {email, username, password} = req.body;
     const user = new User({
         email, 
         username
@@ -26,5 +29,21 @@ router.post('/register', catchAsync(async(req, res) => {
         res.redirect('/users/register');
     }
 }));
+
+// Login
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', 
+passport.authenticate('local', {
+    failureFlash: true, 
+    failureRedirect: '/users/login'
+}), 
+(req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/coops');
+});
 
 module.exports = router;
