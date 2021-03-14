4f52c510cd62971ea1911d8a7df150b4838a3f44
Ricky Yu <rickyyu2294@gmail.com>
2021-03-13 22:22:03 -0800

    Add owner to coop model

--- models/coopModel.js
+++ models/coopModel.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Chicken = require('./chickenModel');
+const User = require('./userModel');
 const Schema = mongoose.Schema;
 
 const CoopSchema = new Schema({
     name: String,
     image: String,
     description: String,
+    owner: {type: Schema.Types.ObjectId, ref: 'User'},
     chickens: [{type: Schema.Types.ObjectId, ref: 'Chicken'}]
 });
 
