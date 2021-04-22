9f3dfcd0af159a1ebc8a583cf54239877c5837a5
Ricky Yu <rickyyu2294@gmail.com>
2021-04-22 00:01:27 -0700

    Move chicken logic to controller

---
+++ controllers/chickens.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Coop = require('../models/coopModel')
+const Chicken = require('../models/chickenModel')
+
+module.exports.new = async (req, res) => {
+    const coop = await Coop.findById(req.params.id);
+    const chicken = new Chicken(req.body.chicken);
+    chicken.owner = req.user._id;
+    coop.chickens.push(chicken);
+    await chicken.save();
+    await coop.save();
+    req.flash('success', 'Successfully added chicken to coop');
+    res.redirect(`/coops/${coop._id}`);
+};
+
+module.exports.delete = async (req, res) => {
+    const {id, chickenid} = req.params;
+    console.log(id);
+    await Coop.findByIdAndUpdate(id, {$pull: {chickens: chickenid}});
+    await Chicken.findByIdAndDelete(chickenid);
+    req.flash('success', 'Successfully removed chicken from coop');
+    res.redirect(`/coops/${id}`);
+};
--- routes/chickenRoutes.js
+++ routes/chickenRoutes.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const Joi = require("joi");
-const ExpressError = require('../utils/ExpressError');
+const chickens = require('../controllers/chickens')
 const router = express.Router({mergeParams: true});
 const catchAsync = require("../utils/CatchAsync");
 const {isLoggedIn, validateChicken, userIsChickenOwner} = require("../utils/middleware");
-const Coop = require('../models/coopModel')
-const Chicken = require('../models/chickenModel')
-const {chickenSchema} = require('../schemas.js');
 
 // Add
-router.post('/', isLoggedIn, validateChicken, catchAsync(async (req, res) => {
-    const coop = await Coop.findById(req.params.id);
-    const chicken = new Chicken(req.body.chicken);
-    chicken.owner = req.user._id;
-    coop.chickens.push(chicken);
-    await chicken.save();
-    await coop.save();
-    req.flash('success', 'Successfully added chicken to coop');
-    res.redirect(`/coops/${coop._id}`);
-}));
+router.post('/', isLoggedIn, validateChicken, catchAsync(chickens.new));
 
 // Delete
-router.delete('/:chickenid/', isLoggedIn, userIsChickenOwner, catchAsync(async (req, res) => {
-    const {id, chickenid} = req.params;
-    console.log(id);
-    await Coop.findByIdAndUpdate(id, {$pull: {chickens: chickenid}});
-    await Chicken.findByIdAndDelete(chickenid);
-    req.flash('success', 'Successfully removed chicken from coop');
-    res.redirect(`/coops/${id}`);
-}));
+router.delete('/:chickenid/', isLoggedIn, userIsChickenOwner, catchAsync(chickens.delete));
 
 module.exports = router;
