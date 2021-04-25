fead1cfab0d9eb2804d7adf1c145587075cdac01
Ricky Yu <rickyyu2294@gmail.com>
2021-04-24 21:04:19 -0700

    Fix indent

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'Coopa',
-    allowed_formats: ['jpeg', 'png', 'jpg']
+        allowed_formats: ['jpeg', 'png', 'jpg']
     }
   });
 
