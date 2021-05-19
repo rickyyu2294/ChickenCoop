4c295649697204843718ccd593e74612b69121ac
Ricky Yu <rickyyu2294@gmail.com>
2021-05-19 00:24:22 -0700

    Add thumbnail virtual to coop schema

--- models/coopModel.js
+++ models/coopModel.js
@@ -3,14 +3,17 @@ const Chicken = require('./chickenModel');
 const User = require('./userModel');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+})
 const CoopSchema = new Schema({
     name: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     description: String,
     owner: {type: Schema.Types.ObjectId, ref: 'User'},
     chickens: [{type: Schema.Types.ObjectId, ref: 'Chicken'}]
